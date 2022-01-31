d73090e3297f2053761eefc40aa4facf000b20d3
Enaium <wyy-666@hotmail.com>
2022-01-31 18:59:13 +0800

    learn3

--- src/main/java/cn/enaium/learn/mixin/program/Main.java
+++ src/main/java/cn/enaium/learn/mixin/program/Main.java
@@ -1,11 +1,17 @@
 package cn.enaium.learn.mixin.program;
 
+import cn.enaium.learn.mixin.mixin.IProgramMixin;
+
 /**
  * Project: Mixin
  * Author: Enaium
  */
 public class Main {
     public static void main(String[] args) {
-        new Program();
+        Program program = new Program();
+        IProgramMixin program1 = (IProgramMixin) program;
+        program1.setName("HELLO");
+        System.out.println(program1.getName());
+        program1.cHello();
     }
 }
--- src/main/java/cn/enaium/learn/mixin/program/Program.java
+++ src/main/java/cn/enaium/learn/mixin/program/Program.java
@@ -5,66 +5,15 @@ package cn.enaium.learn.mixin.program;
  * Author: Enaium
  */
 public class Program {
-    public Program() {
-        Head();
-        Head("Head");
-        Return();
-        Return(1);
-        Tail();
-        Invoke();
-        InvokeAssign();
-        Field();
-        New();
+    private final String name = "Enaium";
+
+    private void hello() {
+        System.out.println("HELLO WORLD!");
     }
 
-    public void Head() {
-
-    }
-
-    public void Head(String name) {
-
-    }
-
-    public void Return() {
-        System.out.println("===RETURN===");
-    }
-
-    public void Return(int index) {
-        System.out.println("===RETURN===");
-        switch (index) {
-            case 0:
-                return;
-            case 1:
-                return;
-            case 2:
-                return;
-        }
-    }
-
-    public void Tail() {
-        System.out.println("===TAIL===");
-    }
-
-    public void Invoke() {
-        System.out.println("===INVOKE===");
-        System.out.println("println 0");
-        System.out.println("println 1");
-    }
-
-    public void InvokeAssign() {
-        System.out.println("===INVOKE_ASSIGN===");
-        "".isEmpty();
-    }
-
-    public static String field = "FIELD";
-
-    public void Field() {
-        System.out.println("===FIELD===");
-        System.out.println(field);
-    }
-
-    public void New() {
-        System.out.println("===NEW===");
-        new String();
-    }
+    //
+// @Invoker principle:
+//    public void cHello() {
+//        hello();
+//    }
 }
